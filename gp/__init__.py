169f2802a35990583ac95bd19374e876f28921cd
Anass Al <dev@anassinator.com>
2018-07-06 22:24:19 -0400

    Fix import error in Python 3

--- gp/__init__.py
+++ gp/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1.0"
 
-import kernels
+from . import kernels
 from .models import GaussianProcess, MultiGaussianProcess
 
 __all__ = ["GaussianProcess", "MultiGaussianProcess", "kernels"]
